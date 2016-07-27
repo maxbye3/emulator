7a0855c7fe87d620a1dd9553e94f07486242b188
Max Bye <maxbye@gmail.com>
2016-07-27 15:06:11 +0100

    emualtor completed

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+$(document).ready(function() {      
+   $('.carousel').carousel({
+    pause: true,
+    interval: false
+    });
+});
+
+
 function giveFeedback(){
     
     var messageType = $(".btn-group .active").html();
     var message = $("textarea").val();
     var name = $("#name").val();
     writeUserData(messageType,message,name);
+}
 
-
+function continueToSite(){
+    $("body").css("padding-top","0px");
+    $(".disclaimer").hide();
+    $(".site").show();
 }
 
 function writeUserData(messageType,message,name) {
@@ -23,7 +35,10 @@ function writeUserData(messageType,message,name) {
 }
 
 function completeFeedback(){
-    $(".contact-section").fadeOut(); 
+    $(".thanks-section").fadeIn();
+    $(".contact-section").fadeOut();
+    $("textarea").val('');
+    $("#name").val('')
 }
 
 function phone(){
@@ -32,13 +47,24 @@ function phone(){
     $(".tablet").css("opacity","1");
     $("body").css("background","#eb6b56");
     $("#frameID").css({"height":"667px","width":"375px"});
+
+    $(".view-type").html("Currently viewing kindness app in phone view.");
+
+    $("body").css("background","#3fb0ac");
+    $(".scrollBtn").css("background","#fae596");
+    $(".scrollBtn").css("color","black");
 }
 
 function tablet(){
     console.log("tablet here");
     $(".phone").css("opacity","1");
     $(".tablet").css("opacity",".5");
-    $("body").css("background","#f37934");
+    $("body").css("background","#9ad3de");
+    $(".scrollBtn").css("background","#3fb0ac");
+    $(".scrollBtn").css("color","white");
+
+     $(".view-type").html("Currently viewing kindness app in tablet view.");
+
     $("#frameID").css({"height":"1024px","width":"768px"});
 }
 
@@ -53,3 +79,8 @@ $( ".btn-default" ).click(function() {
   $(".btn-default").removeClass("active");
   $(this).addClass("active");
 });
+
+function moreFeedback(){
+    $(".thanks-section").fadeOut();
+    $(".contact-section").fadeIn();
+}
